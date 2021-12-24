73d3b14ae301552fd3f5ca237bcdeef46e1013c5
Toramann <toraman042@hotmail.com>
2021-12-24 18:11:04 +0300

    tc01 and tc02 added

--- src/test/java/com/apiTasks/Osman/tests/TC03.java
+++ src/test/java/com/apiTasks/Osman/tests/TC03.java
@@ -1,2 +1,34 @@
-package com.apiTasks.Osman.tests;public class TC03 {
+package com.apiTasks.Osman.tests;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.testng.Assert.*;
+
+/* TC03 --> spartanApi
+        * Given accept type is Json
+        * And ıd parameter value is 5 (do not use this num, use different number)
+        * When user sends GET request to /api/spartans/{id}
+        * Then response status code should be 200
+        * And response content-type: application/json
+        * And "Blythe" should be in the payload --> means body(people likes to use fancy word)
+ */
+
+public class TC03 {
+
+    @Test
+    public void task3(){
+
+        given().pathParam("id",80)
+                .when().get("http://3.86.189.171:8000/api/spartans/{id}");
+
+
+
+
+
+    }
+
+
+
+
 }
