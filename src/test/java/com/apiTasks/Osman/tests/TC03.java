278038b143e7c8f59f1ed5632ca05c340f8a466d
Toramann <toraman042@hotmail.com>
2021-12-26 15:00:14 +0300

    task4

--- src/test/java/com/apiTasks/Osman/tests/TC03.java
+++ src/test/java/com/apiTasks/Osman/tests/TC03.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.testng.Assert.*;
+import static org.hamcrest.Matchers.*;
 
 /* TC03 --> spartanApi
         * Given accept type is Json
@@ -11,7 +12,7 @@ import static org.testng.Assert.*;
         * When user sends GET request to /api/spartans/{id}
         * Then response status code should be 200
         * And response content-type: application/json
-        * And "Blythe" should be in the payload --> means body(people likes to use fancy word)
+        * And "Jocelin" should be in the payload --> means body(people likes to use fancy word)
  */
 
 public class TC03 {
@@ -20,9 +21,11 @@ public class TC03 {
     public void task3(){
 
         given().pathParam("id",80)
-                .when().get("http://3.86.189.171:8000/api/spartans/{id}");
-
-
+                .when().get("http://3.86.189.171:8000/api/spartans/{id}")
+                .then().assertThat().statusCode(200)
+                .and().contentType(equalTo("application/json"))
+                .and().body("name",equalTo("Jocelin"),"id",equalTo(80)
+                ,"gender",equalTo("Female"),"phone",equalTo(4299058649L));
 
 
 
